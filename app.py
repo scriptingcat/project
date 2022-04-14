56d6645caf2f0f97abaecd615f5c69827266ff6c
scriptingcat <antonellagiugliano@gmail.com>
2022-04-14 16:23:44 +0200

    log in and sign up page

--- app.py
+++ app.py
@@ -1,1 +1,142 @@
 #app
+
+import os
+from flask import Flask,redirect, request, render_template, session
+from flask_session import Session
+from tempfile import mkdtemp
+
+from cs50 import SQL
+from werkzeug.security import check_password_hash, generate_password_hash
+from datetime import datetime
+from helpers import login_required, validCharPass, validLenPass, validEmail
+
+# configure application
+app = Flask(__name__)
+
+# make templates auto-reload
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///keeptrack.db")
+
+@app.after_request
+def after_request(response):
+    # ensure responses aren't cached
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+@app.route("/")
+@login_required
+def index():
+    return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    # log user in
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return ("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return ("must provide password", 403)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return ("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    # log user out
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+
+        # get input from user
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmationpassword")
+
+        # check input is not null
+        # create an apology message for each and render it
+        apologymsg = ""
+        if not username and not email and not password and not confirmation:
+            apologymsg = "all fields are required"
+            return render_template("signup.html", apologymsg=apologymsg.capitalize())
+        if not username:
+            apologymsg = "must provide username"
+            return render_template("signup.html", apologymsg=apologymsg.capitalize())
+        if not email:
+            apologymsg = "must provide email"
+            return render_template("signup.html", apologymsg=apologymsg.capitalize())
+        if not validEmail(email):
+            apologymsg = "invalid email format"
+            return render_template("signup.html", apologymsg=apologymsg.capitalize())
+        if not password:
+            apologymsg = "must provide password"
+            return render_template("signup.html", apologymsg=apologymsg.capitalize())
+        # check pass lenght and char
+        if not validLenPass(password) or not validCharPass(password):
+            apologymsg = "password must be at least 8 char long and include at least 1 digits, 1 letter, 1 capital letter and 1 special char among @!#$%^&*?,:"
+            return render_template("signup.html", apologymsg=apologymsg.capitalize())
+        if not confirmation:
+            apologymsg = "must provide confirmation"
+            return render_template("signup.html", apologymsg=apologymsg.capitalize())
+        if not password == confirmation:
+            apologymsg = "password and confirmation must match"
+            return render_template("signup.html", apologymsg=apologymsg.capitalize())
+        
+        # check username and email are not already used
+        rows = db.execute("SELECT * FROM users WHERE username = ?", username)
+        if len(rows) >= 1:
+            apologymsg = "username not available"
+        rows = db.execute("SELECT * FROM users WHERE email = ?", email)
+        if len(rows) >= 1:
+            apologymsg = "email already used"
+        
+        # create the account
+        hash = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
+        db.execute("INSERT INTO users (username, email, hash) VALUES (?, ?, ?)", username, email, hash)  
+
+        # remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+        return redirect("/")
+
+    else:
+        return render_template("signup.html")
--- helpers.py
+++ helpers.py
@@ -1,1 +1,59 @@
 # helpers for main app.py
+import os
+import requests
+import urllib.parse
+import re
+
+from flask import redirect, render_template, request, session
+from functools import wraps
+
+# from finance pset9
+# ensure user is already logged in
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
+# check password is 8 digits min
+def validLenPass(password):
+    if len(password) > 7:
+        return True
+    return False
+
+# check password contains at least 1 letter, 1 capital letter, 1 digit, 1 special char
+def validCharPass (password):
+    specialchar = ["@","!", "#", "$", "%","^", "&", "*", "?", "|", ":"]
+    alpha= 0
+    capital = 0
+    digit = 0
+    special = 0
+    for char in password:
+        if char.isalpha():
+            alpha += 1
+        if char.isupper():
+            capital += 1
+        if char.isdigit():
+            digit += 1
+        if char in specialchar:
+            special += 1
+        if alpha >= 1 and capital >= 1 and digit >= 1 and special >= 1:
+            return True
+    return False
+
+# check valid email expression
+def validEmail(email):
+    # valid regular expression
+    # r at the beginning makes sure it's a raw string
+    # \b makes sure there are any char among []-> \b[] stars with, []\b ends with
+    valid = r'\b[a-zA-Z0-9._*-]+@[a-zA-Z0-9.-]+\.[a-z|A-Z]\b'
+    if re.fullmatch(valid, email):
+        return True
+    return False
